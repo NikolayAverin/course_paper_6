604554aa64fe57bc1ef8a8ee75f1a3b7e3658699
Nikolay Averin <rizzzon116@mail.ru>
2024-05-12 20:05:06 +0400

    finish first part

--- mailing_list_service/settings.py
+++ mailing_list_service/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mailing_list_service',
         'USER': 'postgres',
-        'PASSWORD': '111111',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
